2f95a6e40f22ce2e4dd089306c716502f3cc24b9
itachi0501 <aryankhatri02@gmail.com>
2024-04-29 06:47:49 +0530

    3rd commit

--- an.py
+++ an.py
@@ -9,16 +9,20 @@ def index():
     return render_template('index.html')
 
 # Route to handle image upload and processing
+from flask import request, jsonify
+
 @app.route('/recognize_plate', methods=['POST'])
 def recognize_plate_endpoint():
     if 'image' not in request.files:
         return jsonify({'error': 'No image uploaded'}), 400
 
     image_file = request.files['image']
+    username = request.form.get('username')
+    status = request.form.get('status')
+
     plate_number = recognize_plate(image_file)
-    print('asndasdkjashdkjasbdkjas')
-    print(plate_number)
-    return jsonify({'plate_number': plate_number}), 200
+   
+    return jsonify({'username': username, 'status': status, 'plate_number': plate_number}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
